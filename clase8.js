200747ae2961b54efe2798ff10ad5a9d2a3b42cc
Lien Sanchez <liensilviosanchez@gmail.com>
2022-06-01 22:51:09 -0300

    clase8

--- clase8.js
+++ clase8.js
@@ -3,10 +3,23 @@
 
 const express = require ('express')
 
+//requerimos router
+const {Router} = express
+
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+const PORT = 8080
+
+const server = app.listen(PORT, () => {
+  console.log(`servidor en ${PORT}`)
+})
+
+server.on('error' , error => console.log(`error : ${error}`))
+
 
 let messages = [
   {
@@ -21,6 +34,11 @@ let messages = [
   }
 ]
 
+//nuevo router
+const messagesRouter = Router()
+//ya nnoi es necesario usar app.get, etc
+
+/*REEMPLAZADO X ROUTER
 app.get('/api/mensajes', (req,res)=>{
   
   return res.json(messages)
@@ -36,11 +54,32 @@ app.post('/api/mensajes', (req,res) => {
 
   return res.status(201).json(newMessage)
 })
+*/
 
-const PORT = 8080
+//va el '' vacio xq ya tenemos el path para todos
+messagesRouter.get('', (req,res)=>{
+  
+  return res.json(messages)
 
-const server = app.listen(PORT, () => {
-  console.log(`servidor en ${PORT}`)
 })
 
-server.on('error' , error => console.log(`error : ${error}`))
+messagesRouter.post('', (req,res) => {
+  const newMessage = req.body
+
+  newMessage.id = messages.length + 1
+
+  messages.push(newMessage)
+
+  return res.status(201).json(newMessage)
+})
+
+//asociamos router con app con el path 'principal', router y se quita de linea 50 y56 xq ya esta en app.use
+app.use('/api/mensajes', messagesRouter)
+
+
+
+
+
+
+
+
